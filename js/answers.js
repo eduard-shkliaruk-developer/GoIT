3b1f5184655b8bb753783673d21153cf909bfcec
Eduard <eduard.shkliaruk@globallogic.com>
2022-06-27 18:26:20 +0300

    structure module-2-lesson-3

--- js/answers.js
+++ js/answers.js
@@ -1,2 +1,28 @@
-// Запитання 1.
+// ? Запитання 1.
 
+const message = 'JavaScript это интересно';
+
+// console.log(message.split(' '));
+
+// ? Запитання 2.
+
+// Додавання методом splice(position, 0, new_element_1, new_element_2, ...)
+
+// ? Запитання 3.
+
+// Метод pop()
+
+// ? Запитання 4.
+
+// Чому `i` в квадратних скобках?
+
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] < threshold) {
+//         continue;
+//     }
+
+//     console.log(`Число больше чем ${threshold}: ${numbers[i]}`); // 18, 29, 34
+// }
---
+++ js/script-4.js
@@ -0,0 +1,23 @@
+/**** Методи масиву ****/
+
+// Метод split(delimiter)
+
+// Метод join(delimiter)
+
+// Метод indexOf(value)
+
+// Метод includes(value)
+
+// Метод push(newValue)
+
+// Метод pop()
+
+// Метод slice(begin, end)
+
+// Метод splice(position, num)
+
+// Додавання методом splice(position, 0, new_element_1, new_element_2, ...)
+
+// Заміна методом splice(position, num, new_element_1, new_element_2, ...)
+
+// Метод concat(newArray)
